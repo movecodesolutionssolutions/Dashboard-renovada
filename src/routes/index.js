b53bb076d91ea80813dbd3c5c944c68df678dce9
SamuelFeijo19 <smuelfeijo@gmail.com>
2023-11-12 16:55:59 -0500

    rotas corrigidas

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,58 +5,39 @@ import Registre from "../Pages/Registre-se/index";
 import Recovery from "../Pages/Recovery";
 import User from "../Pages/Users";
 import Header from "../components/Header";
-<<<<<<< HEAD
 import EventsList from "../Pages/Eventos/List";
-
-const Private = ({ Item }) => {
-  const isAuthenticated = true;
-=======
 import { PrivateRoute } from "./privateRoutes";
 
 const HeaderWrapper = () => {
-  const location = useLocation();
->>>>>>> c0fd7a07a5cf184c3ac2832c002062603fdee2b0
+    const location = useLocation();
 
-  const routesWithoutHeader = ["/", "/signup", "/recocery"];
+    const routesWithoutHeader = ["/", "/signup", "/recocery"];
 
-  if (routesWithoutHeader.includes(location.pathname)) {
-    return null;
-  }
+    if (routesWithoutHeader.includes(location.pathname)) {
+        return null;
+    }
 
-  return <Header />;
+    return <Header />;
 };
 
 const RoutesApp = () => {
-  return (
-    <BrowserRouter>
-<<<<<<< HEAD
-      <Header />
-      <Fragment>
-        <Routes>
-            EventsList
-          <Route exact path="/Users" element={<Private Item={User} />} />
-          <Route path="/" element={<Login />} />
-          <Route exact path="/signup" element={<Registre />} />
-          <Route path="*" element={<Login />} />
+    return (
+        <BrowserRouter>
+            <HeaderWrapper />
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route exact path="/signup" element={<Registre />} />
+                <Route exact path="/recocery" element={<Recovery />} />
+                <Route path="/Users" element={<PrivateRoute />}>
+                    <Route exact path="/Users" element={<User />} />
+                </Route>
 
-        {/*INICIO ROTAS EVENTOS*/}
-            <Route exact path="/eventos/listar" element={<Private Item={EventsList} />} />
-        {/*FIM ROTAS EVENTOS*/}
-        </Routes>
-      </Fragment>
-=======
-      <HeaderWrapper />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route exact path="/signup" element={<Registre />} />
-        <Route exact path="/recocery" element={<Recovery />} />
-        <Route path="/Users" element={<PrivateRoute />}>
-          <Route exact path="/Users" element={<User />} />
-        </Route>
-      </Routes>
->>>>>>> c0fd7a07a5cf184c3ac2832c002062603fdee2b0
-    </BrowserRouter>
-  );
+                <Route path="/eventos/listar" element={<PrivateRoute />}>
+                    <Route exact path="/eventos/listar" element={<EventsList />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default RoutesApp;
