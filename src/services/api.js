876af059d03bb9a17d238d44d5ffc781c44f00ae
joaoneto-hub <joaorodrigues8182@gmail.com>
2023-11-16 09:02:30 -0300

    Fix: interceptacao de token

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,17 @@ import axios from "axios";
 export const api = new axios.create({
   baseURL: "https://api-renovada-production.up.railway.app",
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("@Auth:token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
